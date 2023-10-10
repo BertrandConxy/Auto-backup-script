955f75002d17f21edf2c5fbc39ac479903c238a8
Bertrand Mutangana <bertrand@CHOICEs-MacBook-Pro-2.local>
2023-10-10 22:11:12 +0200

    Edit

--- backup.py
+++ backup.py
@@ -7,19 +7,19 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.http import MediaFileUpload
 
-class MyDrive():
+
+class MyDrive:
     def __init__(self):
-        
-        SCOPES = ['https://www.googleapis.com/auth/drive']
+        SCOPES = ["https://www.googleapis.com/auth/drive"]
 
         creds = None
         script_dir = os.path.dirname(os.path.abspath(__file__))
-        credentials_path = os.path.join(script_dir, 'credentials.json')
+        credentials_path = os.path.join(script_dir, "credentials.json")
         # The file token.pickle stores the user's access and refresh tokens, and is
         # created automatically when the authorization flow completes for the first
         # time.
-        if os.path.exists('token.pickle'):
-            with open('token.pickle', 'rb') as token:
+        if os.path.exists("token.pickle"):
+            with open("token.pickle", "rb") as token:
                 creds = pickle.load(token)
         # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
@@ -27,31 +27,37 @@ class MyDrive():
                 creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    credentials_path, SCOPES)
+                    credentials_path, SCOPES
+                )
                 creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
-            with open('token.pickle', 'wb') as token:
+            with open("token.pickle", "wb") as token:
                 pickle.dump(creds, token)
 
-        self.service = build('drive', 'v3', credentials=creds)
+        self.service = build("drive", "v3", credentials=creds)
 
     def find_backup_folder(self):
+        get_folders = (
+            self.service.files()
+            .list(
+                q="name = 'BackupFolder2023' and mimeType='application/vnd.google-apps.folder'",
+                spaces="drive",
+            )
+            .execute()
+        )
 
-        get_folders = self.service.files().list(
-            q="name = 'BackupFolder2023' and mimeType='application/vnd.google-apps.folder'",
-            spaces="drive"
-        ).execute()
-
-        if not get_folders['files']:
+        if not get_folders["files"]:
             folder_metadata = {
                 "name": "BackupFolder2023",
-                "mimeType": "application/vnd.google-apps.folder"
+                "mimeType": "application/vnd.google-apps.folder",
             }
-            parent_folder = self.service.files().create(body=folder_metadata, fields="id").execute()
-            parent_folder_id = parent_folder.get('id')
+            parent_folder = (
+                self.service.files().create(body=folder_metadata, fields="id").execute()
+            )
+            parent_folder_id = parent_folder.get("id")
             return parent_folder_id
         else:
-            parent_folder_id = get_folders['files'][0]['id']
+            parent_folder_id = get_folders["files"][0]["id"]
             return parent_folder_id
 
     def upload_files(self, filename, path):
@@ -67,20 +73,23 @@ class MyDrive():
             print(f"Error: File '{filename}' not found at path '{path}'")
             return  # Exit gracefully
 
-        find_file = self.service.files().list(
-                                        q=f"name='{filename}' and parents='{backup_folder_id}'",
-                                        spaces='drive',
-                                        fields='nextPageToken, files(id, name)',
-                                        pageToken=None
-                                        ).execute()
-        
-        if len(find_file['files']) == 0:
-            file_metadata = {
-                'name': filename,
-                'parents': [backup_folder_id]
-            }
+        find_file = (
+            self.service.files()
+            .list(
+                q=f"name='{filename}' and parents='{backup_folder_id}'",
+                spaces="drive",
+                fields="nextPageToken, files(id, name)",
+                pageToken=None,
+            )
+            .execute()
+        )
 
-            self.service.files().create(body=file_metadata, media_body=file_to_upload, fields='id').execute()
+        if len(find_file["files"]) == 0:
+            file_metadata = {"name": filename, "parents": [backup_folder_id]}
+
+            self.service.files().create(
+                body=file_metadata, media_body=file_to_upload, fields="id"
+            ).execute()
             print(f"{filename} was backed up successfully!")
         else:
             print(f"{filename} is already backed up.")
@@ -88,9 +97,9 @@ class MyDrive():
 
 def main():
     if len(sys.argv) != 2:
-        print("Usage: python backup.py <path_to_directory>")
+        print("Usage: python3 backup.py <path_to_directory>")
         return
-    
+
     path = sys.argv[1]
     expanded_path = os.path.expanduser(path)
     files = os.listdir(expanded_path)
@@ -102,5 +111,5 @@ def main():
     print("Backup is complete!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
